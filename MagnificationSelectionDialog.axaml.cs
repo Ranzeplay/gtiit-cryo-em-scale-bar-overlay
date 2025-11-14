aa669174d6da8b1a695988a4e07feaae903cc312
Jeb Feng <ranzeplay@outlook.com>
2025-11-14 13:58:15 +0800

    Add 36x ratio option and update scale bar nm number

--- MagnificationSelectionDialog.axaml.cs
+++ MagnificationSelectionDialog.axaml.cs
@@ -10,11 +10,12 @@ public partial class MagnificationSelectionDialog : Window
     public List<MagnificationOption> Options { get; } =
     [
         new(11, 13.3f, 500),
-        new(45, 3.1f, 100),
+        new(36, 3.96f, 100),
+        new(45, 3.17f, 100),
         new(57, 2.5f, 100),
         new(73, 1.9f, 100),
-        new(92, 1.5f, 100),
-        new(120, 1.2f, 100),
+        new(92, 1.5f, 50),
+        new(120, 1.2f, 50),
         new(150, 0.95f, 50),
         new(190, 0.74f, 50),
         new(240, 0.58f, 50)
